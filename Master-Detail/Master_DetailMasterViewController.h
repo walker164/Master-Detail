a6f7ebcde618275560aadcb6634d0cf217266f41
walker164 <webstudent@PC16300.local>
2013-10-07 18:31:23 -0500

    My First GitHub

--- Master-Detail/Master_DetailMasterViewController.h
+++ Master-Detail/Master_DetailMasterViewController.h
@@ -2,7 +2,7 @@
 //  Master_DetailMasterViewController.h
 //  Master-Detail
 //
-//  Created by Charles Konkol on 10/6/13.
+//  Created by Sean Gray on 10/7/13.
 //  Copyright (c) 2013 Chuck Konkol. All rights reserved.
 //
 
